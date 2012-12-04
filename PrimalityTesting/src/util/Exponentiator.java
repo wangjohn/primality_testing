a913f6ccb11d73dc3d7b3261a468e3ff63cd787f
Jason Hoch <jrshoch@gmail.com>
2012-12-03 20:46:41 -0500

    Starts some stuff

---
+++ PrimalityTesting/src/util/Exponentiator.java
@@ -0,0 +1,12 @@
+package util;
+
+import java.math.BigInteger;
+
+public class Exponentiator {
+
+	public static BigInteger moduloPower(BigInteger base, BigInteger power,
+			BigInteger modulus) {
+		
+	}
+
+}
